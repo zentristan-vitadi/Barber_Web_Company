7812b41cfe64da2568a563d2844c1a884031d9d0
zentristan vitadi <zentristan.xpro@gmail.com>
2025-11-16 16:31:51 +0700

    Half Of The Website

---
+++ script.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () =>{
+    const position = window.scrollY > 0
+    navbar.classList.toggle("scrolling-active", position)
+}) 
