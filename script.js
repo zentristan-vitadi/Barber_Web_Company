8fe74d76ad99acada19d6ff68ac758d4dd43fcda
zentristan vitadi <zentristan.xpro@gmail.com>
2025-11-18 20:06:55 +0700

    Complete Web

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ window.addEventListener("scroll", () =>{
     const position = window.scrollY > 0
     navbar.classList.toggle("scrolling-active", position)
 }) 
+
+const semua_tombol = document.querySelectorAll(".UpperFooterButton")
+
+semua_tombol.forEach((tombol_satuan) => {
+    tombol_satuan.addEventListener("click", () => {
+        semua_tombol.forEach((btn) => btn.classList.remove("active"));
+        tombol_satuan.classList.add("active");
+    });
+});
